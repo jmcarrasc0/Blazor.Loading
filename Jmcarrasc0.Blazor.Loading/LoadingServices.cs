2597fdf663fcd10cef44939db42b7cc98c15bee4
Juan Carrasco <j_mcarrasco@outlook.com>
2023-08-28 00:32:25 -0400

    Agregar archivos de proyecto.

---
+++ Jmcarrasc0.Blazor.Loading/LoadingServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jmcarrasc0.Blazor.Loading
+{
+    public class LoadingServices
+    {
+            public event Action OnShow;
+            public event Action OnHide;
+
+            public void Show()
+            {
+                OnShow?.Invoke();
+            }
+
+            public void Hide()
+            {
+                OnHide?.Invoke();
+            }
+       
+    }
+}
